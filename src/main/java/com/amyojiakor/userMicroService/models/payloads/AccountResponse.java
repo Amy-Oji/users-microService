1bda2e1e2640da1c2d121b2b185c039b04ba46b3
Amarachukwu Ojiakor <amyojiakor@gmail.com>
2023-04-28 21:03:56 +0100

    Enable UserService to consume Kafka messages from AccountService
    This commit adds the necessary code changes to allow UserService to consume Kafka messages from AccountService when a user creates an account. The KafkaConfig class was created to create the required beans for producing and consuming Kafka messages.
    
    Additionally, the `consume` method in the UserService has been annotated with `@KafkaListener` to listen for messages on the `"${kafka.topic.account-creation}"` topic. When a message is received, the `consume` method retrieves the user associated with the email address in the message payload, creates a new `UserAccounts` object from the message payload, and saves it to the database.
    
    This feature enhances the overall functionality of the microservice project by enabling communication between the User and Account services via Kafka messages.
    
    The changes were tested and no part of the codebase is broken.

--- src/main/java/com/amyojiakor/userMicroService/models/payloads/AccountResponse.java
+++ src/main/java/com/amyojiakor/userMicroService/models/payloads/AccountResponse.java
@@ -1,12 +1,11 @@
 package com.amyojiakor.userMicroService.models.payloads;
 
-
 import com.amyojiakor.userMicroService.models.enums.AccountType;
 import com.amyojiakor.userMicroService.models.enums.CurrencyCode;
 
 import java.math.BigDecimal;
 
-public record AccountResponse (String Email, String accountNumber, String accountName, AccountType accountType, CurrencyCode currencyCode, BigDecimal accountBalance) {
+public record AccountResponse(String email, String accountNumber, String accountName, AccountType accountType, CurrencyCode currencyCode, BigDecimal accountBalance) {
 
 
 }
